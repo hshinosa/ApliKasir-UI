82687e8688b83171dfbfa65cd3488612a799b9c8
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-06-08 17:53:07 +0700

    Mengimplementasikan Secure Code : Sanitize Input pada Fitur Tambah dan Edit Barang

--- ApliKasir-UI/EditBarang.cs
+++ ApliKasir-UI/EditBarang.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -25,9 +26,13 @@ namespace ApliKasir_UI
             DataBarang dataBarang = new DataBarang();
 
             dataBarang.idBarang = this.idBarang;
-            dataBarang.namaBarang = textBoxNama.Text;
-            dataBarang.hargaBarang = double.Parse(textBoxHarga.Text);
-            dataBarang.jumlahBarang = int.Parse(textBoxJumlah.Text);
+            dataBarang.namaBarang = Regex.Replace(textBoxNama.Text.Trim(), "[^a-zA-Z\\s]", "");
+
+            string hargaBarangSanitized = Regex.Replace(textBoxHarga.Text.Trim(), "[^0-9.]", "");
+            dataBarang.hargaBarang = double.TryParse(hargaBarangSanitized, out double parsedHarga) ? parsedHarga : 0.0;
+
+            string jumlahBarangSanitized = Regex.Replace(textBoxJumlah.Text.Trim(), "[^0-9]", "");
+            dataBarang.jumlahBarang = int.TryParse(jumlahBarangSanitized, out int parsedJumlah) ? parsedJumlah : 0;
 
             await Barang.UpdateBarang("https://localhost:7222", idBarang, dataBarang);
             this.Hide();
--- ApliKasir-UI/InputBarang.cs
+++ ApliKasir-UI/InputBarang.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using LibraryKasir;
@@ -27,9 +28,13 @@ namespace ApliKasir_UI
             int lastId = dataBarangList.LastOrDefault()?.idBarang ?? 0; // Get the last ID in the list, or 0 if the list is empty
 
             dataBarang.idBarang = total + 1;
-            dataBarang.namaBarang = textBoxNama.Text;
-            dataBarang.hargaBarang = double.Parse(textBoxHarga.Text);
-            dataBarang.jumlahBarang = int.Parse(textBoxJumlah.Text);
+            dataBarang.namaBarang = Regex.Replace(textBoxNama.Text.Trim(), "[^a-zA-Z\\s]", "");
+            
+            string hargaBarangSanitized = Regex.Replace(textBoxHarga.Text.Trim(), "[^0-9.]", "");
+            dataBarang.hargaBarang = double.TryParse(hargaBarangSanitized, out double parsedHarga) ? parsedHarga : 0.0;
+
+            string jumlahBarangSanitized = Regex.Replace(textBoxJumlah.Text.Trim(), "[^0-9]", "");
+            dataBarang.jumlahBarang = int.TryParse(jumlahBarangSanitized, out int parsedJumlah) ? parsedJumlah : 0;
 
             Barang.CreateBarang("https://localhost:7222", dataBarang);
 
